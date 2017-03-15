dfc65a7c9a2980ad9bc2afedd66cf30e6c0eab81
Dias Abakanov <a.b.dias@mail.ru>
2017-03-15 10:00:05 +0600

    Hello Dias

---
+++ smake/smake/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Dias");
+            Console.ReadLine();
+        }
+    }
+}
